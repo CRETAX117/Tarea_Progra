af1348da64f794351afd375a7d16ff500ebf7d6d
Cretax117 <BrandonJCC117@hotmail..com>
2023-08-10 14:30:15 -0500

    V1.0.4

--- Funciones.h
+++ Funciones.h
@@ -6,4 +6,16 @@ Colocar las funciones principales aca
 /*
 Actividades a realizar
 
+crear dos prototipos 
+-de mayusculas a minusculas (Proceso)
+-de minusculas a mayusculas
+-de mayuscula y minuscula ej )HoLa)
+utilizando recursividad hacer los siguiente ejemplos
+-elevar una base a una potencia
+-dividir dos numeros con restas sucesivas
+-ingresar un frase por teclado y que se mueva hacia arriba y hacia abajo y que se termine cuando se aplaste enter
 */
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+#define MAX_L 100
